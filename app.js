fba35e4ab0c197a990e95a9ae731e39b310a3a02
bot294 <bot.trial001@gmail.com>
2024-09-16 15:47:49 +0500

    streaming

---
+++ app.js
@@ -0,0 +1,14 @@
+// This is a poorly formatted function with linting issues
+function badlyFormattedFunction() {
+  var camelCaseVariable = "This should be snake_case";
+  if (true) {
+    console.log("ok!!! This block is not properly indented");
+  }
+  // Missing JSDoc comment
+  function nestedFunction() {
+    return "This function is missing a JSDoc comment";
+  }
+  return camelCaseVariable;
+}
+
+badlyFormattedFunction();
