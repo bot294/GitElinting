c619b857e07b2ef7740d3c969a02b81b8d49dfb3
bot294 <bot.trial001@gmail.com>
2024-09-16 16:16:56 +0500

    edit in app.

--- app.js
+++ app.js
@@ -10,15 +10,5 @@ function badly_formatted_function() {
   }
   return camel_caseVariable;
 }
-function badlyFormattedFunction() {
-  var camel_caseVariable = "This should be snake_case";
-  if (true) {
-    console.log("ok!!! This block is not properly indented");
-  }
-  // Missing JSDoc comment
-  function nestedFunction() {
-    return "This function is missing a JSDoc comment";
-  }
-  return camel_caseVariable;
-}
+
 badly_formatted_function();
