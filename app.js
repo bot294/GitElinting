d0be483c0e6225736fd79f1905eee15cd847563c
bot294 <bot.trial001@gmail.com>
2024-09-16 16:13:44 +0500

    edit in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // This is a poorly formatted function with linting issues
 function badly_formatted_function() {
-  var camel_case_variable = "This should be snake_case";
+  var camel_caseVariable = "This should be snake_case";
   if (true) {
     console.log("ok!!! This block is not properly indented");
   }
@@ -8,7 +8,17 @@ function badly_formatted_function() {
   function nestedFunction() {
     return "This function is missing a JSDoc comment";
   }
-  return camel_case_variable;
+  return camel_caseVariable;
 }
-
+function badlyFormattedFunction() {
+  var camel_caseVariable = "This should be snake_case";
+  if (true) {
+    console.log("ok!!! This block is not properly indented");
+  }
+  // Missing JSDoc comment
+  function nestedFunction() {
+    return "This function is missing a JSDoc comment";
+  }
+  return camel_caseVariable;
+}
 badly_formatted_function();
