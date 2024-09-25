ecdb459a3e38772e271fbd73cec1b762b4181f6d
bot294 <bot.trial001@gmail.com>
2024-09-25 12:21:43 +0500

    add in new.js

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-new_fun();
+newFun();
 function add(a,b)
 {
     return a+b;
@@ -7,7 +7,7 @@ function sub_fun(a,b)
 {
     return a-b;
 }    
-function new_fun() 
+function newFun() 
 {       
    console.log(add(2,3) + sub_fun(5,4));
 }        
